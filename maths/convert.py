28dcdbcab46dd35ebb2136ed21e3aec8f7836729
CF-Cat-Boy <cf.86.cat@gmail.com>
2025-03-13 14:58:47 +1100

    Update convert.py

--- maths/convert.py
+++ maths/convert.py
@@ -6,17 +6,13 @@ while True:
         number = float(number)
         number = (number/100)
     elif len(number.split(sep=".")) == 2:
-        print(number)
         number = float(number)
-        print(number)
     elif len(number.split(sep="/")) == 2:
         fraction = number
         listt = number.split(sep="/")
         number = float(listt[0])/float(listt[1])
     elif number.isdecimal():
-        print(number)
         number = float(number)
-        print(number)
     percent = number*100
     decimal = number
 
